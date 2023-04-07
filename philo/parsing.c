f12df969aaf7b34fda0af99b60568f08680e517c
Mohammed El Harchi <mel-harc@e2r3p5.1337.ma>
2023-04-07 11:48:19 +0000

    first

--- philo/parsing.c
+++ philo/parsing.c
@@ -1,21 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mel-harc <mel-harc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/07 11:31:30 by mel-harc          #+#    #+#             */
-/*   Updated: 2023/04/07 11:42:36 by mel-harc         ###   ########.fr       */
+/*   Created: 2023/04/07 11:31:25 by mel-harc          #+#    #+#             */
+/*   Updated: 2023/04/07 11:42:08 by mel-harc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-#include <libc.h>
+int	parsing(char *str)
+{
+	int	sign;
 
-int	parsing(char *str);
-int	ft_isdigit(int c);
+	sign = 1;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-' )
+			sign *= -1;
+		str++;
+	}
+	if (sign == -1 || *str == '\0')
+		return (1);
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (1);
+		str++;
+	}
+	return (0);
+}
 
-#endif
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
